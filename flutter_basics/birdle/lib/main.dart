a949073c7c9d6216da20fc9ddeb3a4412a3ccedb
XUE SHENGBO <e1513085@u.nus.edu>
2026-05-28 22:27:27 +0800

    Lesson3 Layout

--- flutter_basics/birdle/lib/main.dart
+++ flutter_basics/birdle/lib/main.dart
@@ -10,17 +10,20 @@ class MainApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Tile('A', HitType.hit
-
-          ),
+        appBar: AppBar(
+          title: Align(
+            alignment: Alignment.center,
+            child: Text('Birdle'),
+          ),  
         ),
+        body: Center(child: GamePage()),
       ),
     );
   }
 }
+
 
 class Tile extends StatelessWidget {
   const Tile(this.letter, this.hitType, {super.key});
@@ -53,3 +56,28 @@ class Tile extends StatelessWidget {
   }
 }
 
+class GamePage extends StatelessWidget {
+  GamePage({super.key});
+  // This object is part of the game.dart file.
+  // It manages wordle logic, and is outside the scope of this tutorial.
+  final Game _game = Game();
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Column(
+        spacing: 5.0,
+        children: [
+          for (final guess in _game.guesses)
+            Row(
+              spacing: 5.0,
+              children: [
+                for (final letter in guess) Tile(letter.char, letter.type),
+              ],
+            ),
+        ],
+      ),
+    );
+  }
+}
